fc9dde5889dcb2d45b326617e477c9637028ea29
exactmike <mike@exactsolutions.biz>
2017-12-19 12:38:32 -0500

    organizing module files

--- ExchangePermissionsExport.psm1
+++ ExchangePermissionsExport.psm1
@@ -82,6 +82,76 @@ function Expand-GroupPermission
         }
     }
 #end Function Expand-GroupPermission
+function Get-CalendarPermission
+{
+    $CalendarPermission = Get-MailboxFolderPermission -Identity ($Mailbox.alias + ':\Calendar') -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | ?{$_.User -notlike "Anonymous" -and $_.User -notlike "Default"} | Select User, AccessRights
+    if (!$CalendarPermission){
+        $Calendar = (($Mailbox.PrimarySmtpAddress.ToString())+ ":\" + (Get-MailboxFolderStatistics -Identity $Mailbox.DistinguishedName -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | where-object {$_.FolderType -eq "Calendar"} | Select-Object -First 1).Name)
+        $CalendarPermission = Get-MailboxFolderPermission -Identity $Calendar -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | ?{$_.User -notlike "Anonymous" -and $_.User -notlike "Default"} | Select User, AccessRights
+    }
+}
+###################################################################
+#Get Permission Functions
+###################################################################
+Function Get-SIDHistoryRecipientHash 
+    {
+        [cmdletbinding()]
+        param
+        (
+            [parameter(Mandatory)]
+            $ActiveDirectoryDrive
+            ,
+            $ExchangeSession
+        )#End param
+
+        Push-Location
+        Write-Log -Message "Operation: Retrieve Mapping for all User Recipients with SIDHistory."
+
+        #Region GetSIDHistoryUsers
+        Set-Location $($ActiveDirectoryDrive.Name + ':\') -ErrorAction Stop
+        Try
+        {
+            $message = "Get AD Users with SIDHistory from AD Drive $($activeDirectoryDrive.Name)"
+            Write-Log -Message $message -EntryType Attempting
+            $sidHistoryUsers = @(Get-Aduser -ldapfilter "(&(legacyExchangeDN=*)(sidhistory=*))" -Properties sidhistory,legacyExchangeDN -ErrorAction Stop)
+            Write-Log -Message $message -EntryType Succeeded
+        }
+        Catch
+        {
+            $myError = $_
+            Write-Log -Message $message -EntryType Failed -ErrorLog
+            Write-Log -Message $myError.tostring() -ErrorLog
+            throw("Failed: $Message")
+        }
+        Pop-Location
+        Write-Log -Message "Got $($sidHistoryUsers.count) Users with SID History from AD $($ActiveDirectoryDrive.name)" -EntryType Notification
+        #EndRegion GetSIDHistoryUsers
+
+        $sidhistoryusercounter = 0
+        $SIDHistoryRecipientHash = @{}
+        Foreach ($shu in $sidhistoryusers)
+        {
+            $sidhistoryusercounter++
+            $message = 'Generating hash of SIDHistory SIDs and Recipient objects...'
+            $ProgressInterval = [int]($($sidhistoryusers.Count) * .01)
+            if ($($sidhistoryusercounter) % $ProgressInterval -eq 0)
+            {
+                Write-Progress -Activity $message -status "Items processed: $($sidhistoryusercounter) of $($sidhistoryusers.Count)" -percentComplete (($sidhistoryusercounter / $($sidhistoryusers.Count))*100)
+            }
+            $splat = @{Identity = $shu.ObjectGuid.guid; ErrorAction = 'SilentlyContinue'} #is this a good assumption?
+            $sidhistoryuserrecipient = $Null
+            $sidhistoryuserrecipient = Invoke-Command -Session $ExchangeSession -ScriptBlock {Get-Recipient @using:splat} -ErrorAction SilentlyContinue
+            If ($null -ne $sidhistoryuserrecipient)
+            {
+                Foreach ($sidhistorysid in $shu.sidhistory)
+                {
+                    $SIDHistoryRecipientHash.$($sidhistorysid.value) = $sidhistoryuserrecipient
+                }#End Foreach
+            }#end If
+        }#End Foreach
+        Write-Output $SIDHistoryRecipientHash
+    }
+#End Get-SIDHistoryRecipientHash
 function New-PermissionExportObject
     {
         [cmdletbinding()]
@@ -151,65 +221,6 @@ function New-PermissionExportObject
         Write-Output -InputObject $PermissionExportObject
     }
 #end function New-PermissionExportObject
-Function Get-SIDHistoryRecipientHash 
-    {
-        [cmdletbinding()]
-        param
-        (
-            [parameter(Mandatory)]
-            $ActiveDirectoryDrive
-            ,
-            $ExchangeSession
-        )#End param
-
-        Push-Location
-        Write-Log -Message "Operation: Retrieve Mapping for all User Recipients with SIDHistory."
-
-        #Region GetSIDHistoryUsers
-        Set-Location $($ActiveDirectoryDrive.Name + ':\') -ErrorAction Stop
-        Try
-        {
-            $message = "Get AD Users with SIDHistory from AD Drive $($activeDirectoryDrive.Name)"
-            Write-Log -Message $message -EntryType Attempting
-            $sidHistoryUsers = @(Get-Aduser -ldapfilter "(&(legacyExchangeDN=*)(sidhistory=*))" -Properties sidhistory,legacyExchangeDN -ErrorAction Stop)
-            Write-Log -Message $message -EntryType Succeeded
-        }
-        Catch
-        {
-            $myError = $_
-            Write-Log -Message $message -EntryType Failed -ErrorLog
-            Write-Log -Message $myError.tostring() -ErrorLog
-            throw("Failed: $Message")
-        }
-        Pop-Location
-        Write-Log -Message "Got $($sidHistoryUsers.count) Users with SID History from AD $($ActiveDirectoryDrive.name)" -EntryType Notification
-        #EndRegion GetSIDHistoryUsers
-
-        $sidhistoryusercounter = 0
-        $SIDHistoryRecipientHash = @{}
-        Foreach ($shu in $sidhistoryusers)
-        {
-            $sidhistoryusercounter++
-            $message = 'Generating hash of SIDHistory SIDs and Recipient objects...'
-            $ProgressInterval = [int]($($sidhistoryusers.Count) * .01)
-            if ($($sidhistoryusercounter) % $ProgressInterval -eq 0)
-            {
-                Write-Progress -Activity $message -status "Items processed: $($sidhistoryusercounter) of $($sidhistoryusers.Count)" -percentComplete (($sidhistoryusercounter / $($sidhistoryusers.Count))*100)
-            }
-            $splat = @{Identity = $shu.ObjectGuid.guid; ErrorAction = 'SilentlyContinue'} #is this a good assumption?
-            $sidhistoryuserrecipient = $Null
-            $sidhistoryuserrecipient = Invoke-Command -Session $ExchangeSession -ScriptBlock {Get-Recipient @using:splat} -ErrorAction SilentlyContinue
-            If ($null -ne $sidhistoryuserrecipient)
-            {
-                Foreach ($sidhistorysid in $shu.sidhistory)
-                {
-                    $SIDHistoryRecipientHash.$($sidhistorysid.value) = $sidhistoryuserrecipient
-                }#End Foreach
-            }#end If
-        }#End Foreach
-        Write-Output $SIDHistoryRecipientHash
-    }
-#End Get-SIDHistoryRecipientHash
 function Get-TrusteeObject
     {
         [CmdletBinding()]
@@ -549,6 +560,9 @@ function Get-SendASPermisssionsViaLocalLDAP
         }#end foreach
     }
 #end function Get-SendASPermissionsViaLocalLDAP
+###################################################################
+#Main/Control Function
+###################################################################
 Function Export-ExchangePermission
     {
         #ToDo
