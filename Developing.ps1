7b282ecc960749ffb0bcdc6a54085932196223ea
exactmike <mike@exactsolutions.biz>
2018-05-09 11:13:14 -0400

    working on GetCalendarPermissions

--- Developing.ps1
+++ Developing.ps1
@@ -2,12 +2,61 @@
 function Get-ForwardingMailbox
 {}
 
-function Get-CalendarPermission
+function GetCalendarPermission
     {
-        $CalendarPermission = Get-MailboxFolderPermission -Identity ($Mailbox.alias + ':\Calendar') -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | ?{$_.User -notlike "Anonymous" -and $_.User -notlike "Default"} | Select User, AccessRights
-        if (!$CalendarPermission){
-            $Calendar = (($Mailbox.PrimarySmtpAddress.ToString())+ ":\" + (Get-MailboxFolderStatistics -Identity $Mailbox.DistinguishedName -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | where-object {$_.FolderType -eq "Calendar"} | Select-Object -First 1).Name)
-            $CalendarPermission = Get-MailboxFolderPermission -Identity $Calendar -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | ?{$_.User -notlike "Anonymous" -and $_.User -notlike "Default"} | Select User, AccessRights
-        }
+        [cmdletbinding()]
+        param
+        (
+            $TargetMailbox
+            ,
+            [System.Management.Automation.Runspaces.PSSession]$ExchangeSession
+            ,
+            [hashtable]$ObjectGUIDHash
+            ,
+            [hashtable]$excludedTrusteeGUIDHash
+            ,
+            [hashtable]$DomainPrincipalHash
+            ,
+            [hashtable]$UnfoundIdentitiesHash
+            ,
+            $ExchangeOrganization
+            ,
+            $HRPropertySet #Property set for recipient object inclusion in object lookup hashtables
+        )
+        GetCallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -Name VerbosePreference
+        $Identity = $TargetMailbox.guid.guid
+        $splat = @{Identity = $Identity + ':\Calendar'}
+        $RawCalendarPermissions = @(
+            try
+            {
+                Invoke-Command -Session $ExchangeSession -ScriptBlock {Get-MailboxFolderPermission @using:splat} -ErrorAction Stop
+            }
+            catch
+            {
+                Write-Verbose -Message "Get-MailboxPermission threw an error with Identity $Identity.  Attempting to find localized folder name instead."
+                try
+                {
+                    $CalendarFolderName = Invoke-Command -errorAction Stop -session $ExchangeSession -ScriptBlock {
+                        Get-MailboxFolderStatistics -Identity $using:Identity
+                    } | Where-Object -FilterScript {$_.FolderType -eq "Calendar"} | Select-Object -First 1 | Select-Object -ExpandProperty Name
+                    if ([string]::IsNullOrWhiteSpace($CalendarFolderName))
+                    {
+                        Write-Verbose -Message "No Calendar Folder found for Identity $Identity"
+                    }
+                    else
+                    {
+                        #try again with the localized folder name
+                        $splat.Identity = $($Identity + ':\' + $CalendarFolderName)
+                        Invoke-Command -Session $ExchangeSession -ScriptBlock {Get-MailboxFolderPermission @using:splat} -ErrorAction Stop
+                    }
+                }
+                catch
+                {
+                    Write-Verbose -Message "Unresolved Error Retrieving Calendar Permissions for Identity $Identity"
+                }
+            }
+        )
+        #filter anon and default permissions
+        $$RawCalendarPermissions | Where-Object -FilterScript {$_.User -notlike "Anonymous" -and $_.User -notlike "Default"}
     }
 #end Get-CalendarPermission
