a2d807553c4fe7985204e9713549d145059df7df
Mike Campbell <mike@campbellnetwork.net>
2022-09-01 11:14:53 -0400

    module framework progress

---
+++ tests/Module.Tests.ps1
@@ -0,0 +1,40 @@
+$Script:ModuleName = 'ExchangePermissionsExport'
+$CommandName = $MyInvocation.MyCommand.Name.Replace('.Tests.ps1', '')
+Write-Information -MessageData "Command is $CommandName" -InformationAction Continue
+Write-Information -MessageData "Module Name is $Script:ModuleName" -InformationAction Continue
+$Script:ProjectRoot = $(Split-Path -Path $PSScriptRoot -Parent)
+Write-Information -MessageData "ProjectRoot is $Script:ProjectRoot" -InformationAction Continue
+$Script:ModuleRoot = $(Join-Path -Path $(Split-Path -Path $PSScriptRoot -Parent) -ChildPath $Script:ModuleName)
+Write-Information -MessageData "Module Root is $script:ModuleRoot" -InformationAction Continue
+$Script:ModuleFile = $Script:ModuleFile = Join-Path -Path $($Script:ModuleRoot) -ChildPath $($Script:ModuleName + '.psm1')
+Write-Information -MessageData "Module File is $($script:ModuleFile)" -InformationAction Continue
+$Script:ModuleSettingsFile = Join-Path -Path $($Script:ModuleRoot) -ChildPath $($Script:ModuleName + '.psd1')
+Write-Information -MessageData "Module Settings File is $($script:ModuleSettingsFile)" -InformationAction Continue
+
+Describe "$ModuleName Unit Tests" -Tag 'UnitTests' {
+    Context 'Validate Top Level Files' {
+        [string[]]$moduleFileNames = (Get-ChildItem $ModuleRoot -File).Name
+        $expectedFileNames = @($($ModuleName + '.psd1'), $($ModuleName + '.psm1'))
+        It "Should contain expected files $($expectedFileNames -join ', ')" {
+            ( (Compare-Object -ReferenceObject $expectedFileNames -DifferenceObject $moduleFileNames -IncludeEqual | Where-Object SideIndicator -EQ '==').Count ) | Should Be $expectedFileNames.Count
+        }
+        <#         It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        } #>
+    }
+}
+Describe "$ModuleName Project Unit Tests" -Tag 'UnitTests' {
+    Context 'Validate Top Level Files' {
+        [string[]]$ProjectFileNames = (Get-ChildItem $ProjectRoot -File).Name
+        $expectedFileNames = @($('README.md', 'license', 'ScriptAnalyzerSettings.psd1'))
+        It "Should contain expected files $($expectedFileNames -join ', ')" {
+            ( (Compare-Object -ReferenceObject $expectedFileNames -DifferenceObject $ProjectFileNames -IncludeEqual | Where-Object SideIndicator -EQ '==').Count ) | Should Be $expectedFileNames.Count
+        }
+        <#         It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        } #>
+    }
+}
+
+#Write-Information -MessageData "Removing Module $Script:ModuleName" -InformationAction Continue
+#Remove-Module -Name $Script:ModuleName -Force -ErrorAction SilentlyContinue
---
+++ tests/Pester.Tests.ps1
@@ -0,0 +1,32 @@
+$Script:ModuleName = 'ExchangePermissionsExport'
+$CommandName = $MyInvocation.MyCommand.Name.Replace('.Tests.ps1', '')
+Write-Information -MessageData "Command is $CommandName" -InformationAction Continue
+Write-Information -MessageData "Module Name is $Script:ModuleName" -InformationAction Continue
+$Script:ProjectRoot = $(Split-Path -Path $PSScriptRoot -Parent)
+Write-Information -MessageData "ProjectRoot is $Script:ProjectRoot" -InformationAction Continue
+$Script:ModuleRoot = $(Join-Path -Path $(Split-Path -Path $PSScriptRoot -Parent) -ChildPath $Script:ModuleName)
+Write-Information -MessageData "Module Root is $script:ModuleRoot" -InformationAction Continue
+$Script:ModuleFile = $Script:ModuleFile = Join-Path -Path $($Script:ModuleRoot) -ChildPath $($Script:ModuleName + '.psm1')
+Write-Information -MessageData "Module File is $($script:ModuleFile)" -InformationAction Continue
+$Script:ModuleSettingsFile = Join-Path -Path $($Script:ModuleRoot) -ChildPath $($Script:ModuleName + '.psd1')
+Write-Information -MessageData "Module Settings File is $($script:ModuleSettingsFile)" -InformationAction Continue
+
+Write-Information -MessageData "Removing Module $Script:ModuleName" -InformationAction Continue
+Remove-Module -Name $Script:ModuleName -Force -ErrorAction SilentlyContinue
+Write-Information -MessageData "Import Module $Script:ModuleName" -InformationAction Continue
+Import-Module -Name $Script:ModuleSettingsFile -Force
+
+Describe 'Public commands have Pester tests' -Tag 'Build' {
+    $commands = Get-Command -Module $Script:ModuleName
+
+    foreach ($command in $commands.Name)
+    {
+        $file = Get-ChildItem -Path "$Script:ProjectRoot\Tests" -Include "$command.Tests.ps1" -Recurse
+        It "Should have a Pester test for [$command]" {
+            $file.FullName | Should Not BeNullOrEmpty
+        }
+    }
+}
+
+Write-Information -MessageData "Removing Module $Script:ModuleName" -InformationAction Continue
+Remove-Module -Name $Script:ModuleName -Force -ErrorAction SilentlyContinue
