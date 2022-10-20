1fbdd03bae58cdaf9877861f749ec1bc32710563
Mike Campbell <mike@campbellnetwork.net>
2022-10-20 11:09:54 -0400

    fix permission id

--- ExchangePermissionsExport/functions/NewPermissionExportObject.ps1
+++ ExchangePermissionsExport/functions/NewPermissionExportObject.ps1
@@ -44,7 +44,7 @@ Function NewPermissionExportObject
     )#End Param
     $PermissionExportObject =
     [pscustomobject]@{
-        PermissionIdentity          = [guid]::new()
+        PermissionIdentity          = $([guid]::NewGuid()).guid
         ParentPermissionIdentity    = $ParentPermissionIdentity
         SourceExchangeOrganization  = $SourceExchangeOrganization
         TargetObjectGUID            = $TargetMailbox.Guid.Guid
